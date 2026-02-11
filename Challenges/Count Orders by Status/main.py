5bb45d5e6419929691ae25e9143dd230af127965
Colin Gorman <colinrhysgorman@gmail.com>
2026-02-11 21:49:52 +0000

    Refactored solution
    removed nested loops as status key is known and no need to search every key

--- Challenges/Count Orders by Status/main.py
+++ Challenges/Count Orders by Status/main.py
@@ -2,12 +2,10 @@ def count_orders_by_status(orders):
     orders_by_status = {}
 
     for order in orders:
-        for k in order:
-            if k == "status":
-                status = order[k]
-                if status in orders_by_status:
-                    orders_by_status[status] += 1
-                else:
-                    orders_by_status[status] = 1
+        status = order["status"]
+        if status in orders_by_status:
+            orders_by_status[status] += 1
+        else:
+            orders_by_status[status] = 1
 
     return orders_by_status
