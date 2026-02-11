321c08616a2fa5704f61986f1295a94bf72d0cae
Colin Gorman <colinrhysgorman@gmail.com>
2026-02-11 21:46:19 +0000

    Completed 'Count Orders by Status' Challenge

--- Challenges/Count Orders by Status/main.py
+++ Challenges/Count Orders by Status/main.py
@@ -1,2 +1,13 @@
 def count_orders_by_status(orders):
-    pass
+    orders_by_status = {}
+
+    for order in orders:
+        for k in order:
+            if k == "status":
+                status = order[k]
+                if status in orders_by_status:
+                    orders_by_status[status] += 1
+                else:
+                    orders_by_status[status] = 1
+
+    return orders_by_status
