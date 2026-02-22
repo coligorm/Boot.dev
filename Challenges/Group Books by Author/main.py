c7e65bd9a074267c4c75dec9afba3a11d0eaf544
Colin Gorman <colinrhysgorman@gmail.com>
2026-02-22 19:35:29 +0000

    Completed 'Group Books by Author1 Challenge

--- Challenges/Group Books by Author/main.py
+++ Challenges/Group Books by Author/main.py
@@ -1,2 +1,13 @@
 def group_books_by_author(books):
-    pass
+    author_to_titles = {}
+    
+    for book in books:
+        author = book["author"]
+        title = book["title"]
+        
+        if author not in author_to_titles:
+            author_to_titles[author] = []
+            
+        author_to_titles[author].append(title)
+            
+    return author_to_titles
