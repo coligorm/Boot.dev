43240ec9802ae3397b0c103716f0ccc331400bd9
Colin Gorman <colinrhysgorman@gmail.com>
2026-03-05 23:45:36 +0000

    Refactored Challenge 'Token Pouch (DRY OOP)'

--- Challenges/Token Pouch (DRY OOP)/main.py
+++ Challenges/Token Pouch (DRY OOP)/main.py
@@ -1,42 +1,37 @@
 class TokenPouch:
     def __init__(self, starting_tokens):
-        self.starting_tokens = starting_tokens
+        self.__validate_amount(starting_tokens)
+        self.__tokens = starting_tokens
 
     @property
     def tokens(self):
-        return self.starting_tokens
+        return self.__tokens
 
     def deposit(self, amount):
         self.__validate_amount(amount)
-        self.starting_tokens += amount
-        return self.starting_tokens
+        self.__tokens += amount
+        return self.__tokens
 
     def withdraw(self, amount):
         self.__validate_amount(amount)
-        self.__validate_funds(self.starting_tokens, amount)
-        self.starting_tokens -= amount
-        return self.starting_tokens
+        self.__validate_funds(amount)
+        self.__tokens -= amount
+        return self.__tokens
 
     def transfer_to(self, other_pouch, amount):
-        if type(other_pouch) != TokenPouch:
-            raise TypeError
+        if type(other_pouch) is not TokenPouch:
+            raise TypeError("other_pouch must be a TokenPouch")
         self.__validate_amount(amount)
-        self.__validate_funds(self.starting_tokens, amount)
-        self.starting_tokens -= amount
-        other_pouch.starting_tokens += amount
-        return (self.starting_tokens, other_pouch.starting_tokens)
+        self.__validate_funds(amount)
+        self.__tokens -= amount
+        other_pouch.__tokens += amount
+        return (self.__tokens, other_pouch.__tokens)
             
-
     def __validate_amount(self, amount):
-        if type(amount) != int or amount <= 0:
-            raise ValueError
-        else:
-            return True
+        if type(amount) is not int or amount <= 0:
+            raise ValueError("amount must be an int > 0")
 
-    def __validate_funds(self, tokens, amount):
-        if tokens < amount:
-            raise ValueError
-        else:
-            return True
-        
-        
+    def __validate_funds(self, amount):
+        if self.__tokens < amount:
+            raise ValueError("insufficient tokens")
+ 
