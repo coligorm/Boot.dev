764be2a34cf65a16dd8f92165672f8137fca8f05
Colin Gorman <colinrhysgorman@gmail.com>
2026-03-09 23:41:29 +0000

    Completed ' Fix Animal Pack Polymorphism' Challenge

--- Challenges/Fix Animal Pack Polymorphism/main.py
+++ Challenges/Fix Animal Pack Polymorphism/main.py
@@ -3,16 +3,19 @@ class Animal:
         self.name = name
 
     def speak(self):
-        return "..."
+        pass
 
     def __str__(self):
-        return f"{self.name}: Animal"
+        return f"{self.__class__.__name__}: {self.name}"
 
     def __eq__(self, other):
-        return self.name == other.name
+        return self.name == other.name and self.__class__.__name__ == other.__class__.__name__
 
 
 class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+    
     def speak(self):
         return "woof"
 
@@ -32,10 +35,15 @@ class Pack:
         self.animals = animals
 
     def __add__(self, other):
-        return Pack([self.animals, other.animals])
+        new_pack = []
+        for animal in self.animals:
+            new_pack.append(animal)
+        for animal in other.animals:
+            new_pack.append(animal)
+        return Pack(new_pack)
 
     def sounds(self):
         lines = []
         for animal in self.animals:
-            lines.append(f"{animal} says {Animal.speak(animal)}")
+            lines.append(f"{animal} says {animal.speak()}")
         return lines
