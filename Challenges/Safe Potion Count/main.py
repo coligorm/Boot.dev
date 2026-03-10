632e0097565f768dbdba77ab2eee9f5adcca1659
Colin Gorman <colinrhysgorman@gmail.com>
2026-03-10 22:11:13 +0000

    Completed 'Safe Potion Count' Challenge

--- Challenges/Safe Potion Count/main.py
+++ Challenges/Safe Potion Count/main.py
@@ -1,2 +1,10 @@
 def read_potion_count(text):
-    pass
+    try:
+        potion_count = int(text)
+    except ValueError:
+        raise ValueError("not a number")
+        
+    if potion_count < 0:
+        raise ValueError("count cannot be negative")
+    else:
+        return potion_count
