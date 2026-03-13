d3267d100945dad4a73b0607466a83eea3dc950c
Colin Gorman <colinrhysgorman@gmail.com>
2026-03-13 23:54:00 +0000

    Completed 'Calculate Item Revenue' Challenge

--- Challenges/Calculate Item Revenue/main.py
+++ Challenges/Calculate Item Revenue/main.py
@@ -1,2 +1,12 @@
 def get_revenue_per_item(orders, prices):
-    pass
+    results = {}
+
+    for order in orders:
+        for item in order["items"]:
+            if item["name"] in prices:
+                revenue = item["quantity"] * prices[item["name"]]
+                if item["name"] not in results:
+                    results[item["name"]] = revenue
+                else:
+                    results[item["name"]] += revenue
+    return results
