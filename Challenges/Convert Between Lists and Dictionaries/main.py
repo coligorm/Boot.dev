579ed80d956e921c8b72e27b559317cb6f1a5ed9
Colin Gorman <colinrhysgorman@gmail.com>
2026-03-14 23:01:57 +0000

    Completed 'Convert Between Lists and Dictionaries' Challenge

--- Challenges/Convert Between Lists and Dictionaries/main.py
+++ Challenges/Convert Between Lists and Dictionaries/main.py
@@ -1,6 +1,12 @@
 def list_to_dict(pairs):
-    pass
+    dict = {}
+    for pair in pairs:
+        dict[pair[0]] = pair[1]
+    return dict
 
 
 def dict_to_list(mapping):
-    pass
+    l = []
+    for key in mapping:
+        l.append([key, mapping[key]])
+    return l
