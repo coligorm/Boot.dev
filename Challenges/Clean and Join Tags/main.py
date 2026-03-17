facc22f564d72a5b1dcd66a276a58c0963a50423
Colin Gorman <colinrhysgorman@gmail.com>
2026-03-17 19:28:41 +0000

    Completed 'Clean and Join Tags' Challenge

--- Challenges/Clean and Join Tags/main.py
+++ Challenges/Clean and Join Tags/main.py
@@ -13,7 +13,12 @@ LABEL_TOTAL = "Total: "
 SEPARATOR = ", "
 
 def clean_tags(tags):
-    pass
+    cleaned_tags = []
+    for tag in tags:
+        tag = tag.strip().lower()
+        if tag not in cleaned_tags:
+            cleaned_tags.append(tag)
+    return cleaned_tags
 
 cleaned = clean_tags(RAW_TAGS)
 print(LABEL_TAGS + SEPARATOR.join(cleaned))
