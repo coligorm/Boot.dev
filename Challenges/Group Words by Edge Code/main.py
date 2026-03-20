eff8d83ec5a5d784c387c658cdd1de26821eed8c
Colin Gorman <colinrhysgorman@gmail.com>
2026-03-20 21:37:12 +0000

    Completed 'Group Words by Edge Code' Challenge

--- Challenges/Group Words by Edge Code/main.py
+++ Challenges/Group Words by Edge Code/main.py
@@ -1,2 +1,13 @@
 def group_words_by_edge(words):
-    pass
+    edge_letters = {}
+    if len(words) == 0:
+        return edge_letters
+
+    for word in words:
+        edge = word[:1] + word[-1:]
+        if edge not in edge_letters:
+            edge_letters[edge] = [word]
+        else:
+            edge_letters[edge].append(word)
+
+    return edge_letters
