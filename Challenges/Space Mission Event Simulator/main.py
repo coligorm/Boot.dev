4c761c2c611929a818a734d025991b9ff25c3155
Colin Gorman <colinrhysgorman@gmail.com>
2026-03-22 20:39:26 +0000

    Completed 'Space Mission Event Simulator' Challenge

--- Challenges/Space Mission Event Simulator/main.py
+++ Challenges/Space Mission Event Simulator/main.py
@@ -6,24 +6,47 @@ class Ship:
         self.cargo = {}
 
     def mine(self, item, amount, energy_cost):
-        pass
+        if self.energy - energy_cost < 0:
+            return f"Skipped {item}"
+        else:
+            self.energy -= energy_cost
+            if item not in self.cargo:
+                self.cargo[item] = amount
+            else:
+                self.cargo[item] += amount
+            return f"Mined {item} x{amount}"
 
     def take_hit(self, damage):
-        pass
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0 
+        return f"Took {damage} damage"
 
     def charge(self, amount):
-        pass
+        self.energy += amount
+        return f"Charged {amount} energy"
 
     def repair(self, amount):
-        pass
+        self.health += amount
+        if self.health > 100:
+            self.health = 100
+        return f"Repaired {amount} health"
 
     def is_destroyed(self):
-        pass
+        if self.health <= 0:
+            return "Ship destroyed"
 
     def cargo_text(self):
-        pass
+        result = ""
+        if len(self.cargo) == 0:
+            result = "empty, "
 
+        for i in self.cargo:
+            result += f"{i}:{self.cargo[i]}, "
 
+        return result[:-2]
+
+        
 class Mission:
     def __init__(self, ship, events):
         self.ship = ship
@@ -31,8 +54,31 @@ class Mission:
         self.log = []
 
     def run(self):
-        pass
-
+        for event in self.events:
+            match event["type"]:
+                case "mine":
+                    self.log.append(self.ship.mine(event["item"], event["amount"], event["energy"]))
+                case "hit":
+                    self.log.append(self.ship.take_hit(event["damage"]))
+                    if self.ship.is_destroyed() == "Ship destroyed":
+                        self.log.append(self.ship.is_destroyed())
+                        break
+                case "charge":
+                    self.log.append(self.ship.charge(event["energy"]))
+                case "repair":
+                    self.log.append(self.ship.repair(event["health"]))
 
 def simulate_mission(name, events):
-    pass
+    summary = {}
+    ship = Ship(name)
+    mission = Mission(ship, events)
+    mission.run()
+    
+    summary["pilot"] = name
+    summary["health"] = ship.health
+    summary["energy"] = ship.energy
+    summary["cargo"] = ship.cargo
+    summary["cargo_text"] = ship.cargo_text()
+    summary["log"] = mission.log
+
+    return summary
